hawaii-desktop/greenisland

Add initial XWayland QML plugin
Start porting the old code to the new API as a QML plugin.
This will never be complete in time for 0.8 but I want to do
some work ahead.

It barely work with GNOME applications and Spotify, you can see
and move windows that is.

The QtQuick integration is modeled after the new QtWaylandCompositor
API in that there is a shell surface , a QtQuick representation
and an integration logic class.

The X11 window manager creates the shell surface and assign a
surface to it the moment it is available, then a signal is emitted
so that the compositor creates the QtQuick representation that in
turn creates the integration module.

When an event such as move and resize is received by the X11
window manager a signal from the shell surface is emitted and
received by the integration module that performs the action.

declarative/xwayland/XWaylandWindowChrome.qml
@@ -1,0 +1,74 @@
+/****************************************************************************
+ * This file is part of Hawaii.
+ *
+ * Copyright (C) 2016 Pier Luigi Fiorini
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * $BEGIN_LICENSE:LGPL$
+ *
+ * This file may be used under the terms of the GNU Lesser General Public
+ * License version 2.1 or later as published by the Free Software Foundation
+ * and appearing in the file LICENSE.LGPLv21 included in the packaging of
+ * this file.  Please review the following information to ensure the
+ * GNU Lesser General Public License version 2.1 requirements will be
+ * met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+ *
+ * Alternatively, this file may be used under the terms of the GNU General
+ * Public License version 2.0 or later as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPLv2 included in the
+ * packaging of this file.  Please review the following information to ensure
+ * the GNU General Public License version 2.0 requirements will be
+ * met: http://www.gnu.org/licenses/gpl-2.0.html.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
+import QtQuick 2.0
+import QtGraphicalEffects 1.0
+import GreenIsland.XWayland 1.0
+
+XWaylandShellSurfaceItem {
+    property bool decorated: true
+
+    id: windowChrome
+    onSurfaceDestroyed: windowChrome.destroy()
+
+    // Shadow
+    // FIXME: Transparent backgrounds will be opaque due to shadows
+    RectangularGlow {
+        id: dropShadow
+        anchors.fill: parent
+        glowRadius: 10
+        spread: 0.2
+        color: "#80000000"
+        z: -1
+        visible: decorated && !shellSurface.maximized
+    }
+
+    /*
+     * Behavior
+     */
+
+    Behavior on width {
+        SmoothedAnimation {
+            easing.type: Easing.OutQuad
+            duration: 350
+        }
+    }
+
+    Behavior on height {
+        SmoothedAnimation {
+            easing.type: Easing.OutQuad
+            duration: 350
+        }
+    }
+
+    Behavior on scale {
+        SmoothedAnimation {
+            easing.type: Easing.OutQuad
+            duration: 350
+        }
+    }
+}

declarative/xwayland/plugin.cpp
@@ -1,0 +1,56 @@
+/****************************************************************************
+ * This file is part of Hawaii.
+ *
+ * Copyright (C) 2016 Pier Luigi Fiorini
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * $BEGIN_LICENSE:LGPL$
+ *
+ * This file may be used under the terms of the GNU Lesser General Public
+ * License version 2.1 or later as published by the Free Software Foundation
+ * and appearing in the file LICENSE.LGPLv21 included in the packaging of
+ * this file.  Please review the following information to ensure the
+ * GNU Lesser General Public License version 2.1 requirements will be
+ * met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+ *
+ * Alternatively, this file may be used under the terms of the GNU General
+ * Public License version 2.0 or later as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPLv2 included in the
+ * packaging of this file.  Please review the following information to ensure
+ * the GNU General Public License version 2.0 requirements will be
+ * met: http://www.gnu.org/licenses/gpl-2.0.html.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
+#include <QtQml/QQmlExtensionPlugin>
+#include <QtQml/QQmlComponent>
+
+
+#include "xwayland.h"
+#include "xwaylandserver.h"
+#include "xwaylandshellsurface.h"
+#include "xwaylandquickshellsurfaceitem.h"
+
+class XWaylandPlugin : public QQmlExtensionPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface/1.0")
+public:
+    void registerTypes(const char *uri);
+};
+
+void XWaylandPlugin::registerTypes(const char *uri)
+{
+    // @uri GreenIsland.XWayland
+    Q_ASSERT(QLatin1String(uri) == QLatin1String("GreenIsland.XWayland"));
+
+    qmlRegisterType<XWayland>(uri, 1, 0, "XWayland");
+    qmlRegisterUncreatableType<XWaylandShellSurface>(uri, 1, 0, "XWaylandWindow",
+                                               QObject::tr("Cannot create instance of XWaylandWindow"));
+    qmlRegisterType<XWaylandQuickShellSurfaceItem>(uri, 1, 0, "XWaylandShellSurfaceItem");
+}
+
+#include "plugin.moc"

liri-archive/qml-xwayland

src/xwayland/XWaylandWindowChrome.qml
@@ -1,0 +1,74 @@
+/****************************************************************************
+ * This file is part of Liri.
+ *
+ * Copyright (C) 2016 Pier Luigi Fiorini
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * $BEGIN_LICENSE:LGPL$
+ *
+ * This file may be used under the terms of the GNU Lesser General Public
+ * License version 2.1 or later as published by the Free Software Foundation
+ * and appearing in the file LICENSE.LGPLv21 included in the packaging of
+ * this file.  Please review the following information to ensure the
+ * GNU Lesser General Public License version 2.1 requirements will be
+ * met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+ *
+ * Alternatively, this file may be used under the terms of the GNU General
+ * Public License version 2.0 or later as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPLv2 included in the
+ * packaging of this file.  Please review the following information to ensure
+ * the GNU General Public License version 2.0 requirements will be
+ * met: http://www.gnu.org/licenses/gpl-2.0.html.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
+import QtQuick 2.0
+import QtGraphicalEffects 1.0
+import Liri.XWayland 1.0
+
+XWaylandShellSurfaceItem {
+    property bool decorated: true
+
+    id: windowChrome
+    onSurfaceDestroyed: windowChrome.destroy()
+
+    // Shadow
+    // FIXME: Transparent backgrounds will be opaque due to shadows
+    RectangularGlow {
+        id: dropShadow
+        anchors.fill: parent
+        glowRadius: 10
+        spread: 0.2
+        color: "#80000000"
+        z: -1
+        visible: decorated && !shellSurface.maximized
+    }
+
+    /*
+     * Behavior
+     */
+
+    Behavior on width {
+        SmoothedAnimation {
+            easing.type: Easing.OutQuad
+            duration: 350
+        }
+    }
+
+    Behavior on height {
+        SmoothedAnimation {
+            easing.type: Easing.OutQuad
+            duration: 350
+        }
+    }
+
+    Behavior on scale {
+        SmoothedAnimation {
+            easing.type: Easing.OutQuad
+            duration: 350
+        }
+    }
+}

src/xwayland/plugin.cpp
@@ -1,0 +1,56 @@
+/****************************************************************************
+ * This file is part of Liri.
+ *
+ * Copyright (C) 2016 Pier Luigi Fiorini
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * $BEGIN_LICENSE:LGPL$
+ *
+ * This file may be used under the terms of the GNU Lesser General Public
+ * License version 2.1 or later as published by the Free Software Foundation
+ * and appearing in the file LICENSE.LGPLv21 included in the packaging of
+ * this file.  Please review the following information to ensure the
+ * GNU Lesser General Public License version 2.1 requirements will be
+ * met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+ *
+ * Alternatively, this file may be used under the terms of the GNU General
+ * Public License version 2.0 or later as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPLv2 included in the
+ * packaging of this file.  Please review the following information to ensure
+ * the GNU General Public License version 2.0 requirements will be
+ * met: http://www.gnu.org/licenses/gpl-2.0.html.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
+#include <QtQml/QQmlExtensionPlugin>
+#include <QtQml/QQmlComponent>
+
+
+#include "xwayland.h"
+#include "xwaylandserver.h"
+#include "xwaylandshellsurface.h"
+#include "xwaylandquickshellsurfaceitem.h"
+
+class LiriXWaylandPluging : public QQmlExtensionPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface/1.0")
+public:
+    void registerTypes(const char *uri);
+};
+
+void LiriXWaylandPluging::registerTypes(const char *uri)
+{
+    // @uri Liri.XWayland
+    Q_ASSERT(QLatin1String(uri) == QLatin1String("Liri.XWayland"));
+
+    qmlRegisterType<XWayland>(uri, 1, 0, "XWayland");
+    qmlRegisterUncreatableType<XWaylandShellSurface>(uri, 1, 0, "XWaylandWindow",
+                                               QObject::tr("Cannot create instance of XWaylandWindow"));
+    qmlRegisterType<XWaylandQuickShellSurfaceItem>(uri, 1, 0, "XWaylandShellSurfaceItem");
+}
+
+#include "plugin.moc"